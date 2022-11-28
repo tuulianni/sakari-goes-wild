46ee6e3eabf734a15fbdad30c7030d8390763133
tuulianni <tuuli.marjomaa@jappis.fi>
2022-11-28 21:50:41 +0200

    Major breakthrough with sweatercounter

--- sakari-game/Assets/Scripts/GameManager.cs
+++ sakari-game/Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public int sweatersFound = 0;
+    public static int sweatersFound = 0;
     string[] SceneArr;
     // Start is called before the first frame update
     void Start()
@@ -18,4 +18,10 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+		public static void SweaterCounter() {
+
+			sweatersFound++;
+			Debug.Log("SweatersFound: " + sweatersFound);
+		}
 }
