ab19b50a549faeed58cfc43cb352861d74437778
tuulianni <tuuli.marjomaa@jappis.fi>
2022-11-30 22:56:17 +0200

    Added sweatercounter after Laby

--- sakari-game/Assets/Scripts/BackToSakariHouse.cs
+++ sakari-game/Assets/Scripts/BackToSakariHouse.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class BackToSakariHouse : MonoBehaviour
 {
-		
+		public TextMeshProUGUI count;
     // Start is called before the first frame update
     void Start()
     {
+				count.text = "+" + (GameManager.count).ToString();
 				Invoke("Action", 8.0f);
     }
 
 		public void Action()
     {
         SceneManager.LoadScene("HouseSakari");
+				GameManager.count = 0;
 
     }
 
