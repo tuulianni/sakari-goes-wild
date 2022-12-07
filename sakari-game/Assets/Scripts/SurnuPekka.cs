2fb1c53226c7b61d4896d912c64f93f3c62744e0
tuulianni <tuuli.marjomaa@jappis.fi>
2022-12-07 13:46:37 +0200

    Conversation

--- sakari-game/Assets/Scripts/SurnuPekka.cs
+++ sakari-game/Assets/Scripts/SurnuPekka.cs
@@ -12,14 +12,19 @@ public class SurnuPekka : MonoBehaviour
 		//tässä väliaikaisesti tämä
 		public TextMeshProUGUI sweaterAmount;
 
+		//tämä on keskusteluun liittyvä laskuri
+		public int x;
+
+		public TextMeshProUGUI conversationTXT;
+		public GameObject cont;
+
+
     // Start is called before the first frame update
     void Start()
     {
 
-
 			//hello.SetActive(false);
 			conversation.SetActive(false);
-
 			sweaterAmount.text = (GameManager.sweatersFound).ToString();
         
     }
@@ -37,7 +42,45 @@ public class SurnuPekka : MonoBehaviour
 
 		 IEnumerator waiter() 
 		 {
-		 	yield return new WaitForSeconds(5);
+		 	yield return new WaitForSeconds(1);
 		 	conversation.SetActive(true);
+			Conversation();
 		 }
+
+		void Conversation() {
+
+			switch (x)
+			{
+				case 0:
+				conversationTXT.text = "Terve Sakari! Olen täällä puutarhalla!";
+				break;
+
+				case 1:
+				conversationTXT.text = "Terve Surnu-Pekka! Onkos sinulla huolia?";
+				break;
+
+				case 2:
+				conversationTXT.text = "Voi kuule, kyllä on! Ne p## myyrät on taas heränneet ja syövät kaikki naurikseni. ";
+				break;
+
+				case 3:
+				conversationTXT.text = "Voi vitsi, koitappas pärjäillä!";
+				cont.SetActive(false);
+				break;
+				
+				default:
+				conversationTXT.text = "juupajuu";
+				break;
+			}
+		}
+
+		public void ClickHappened() {
+			x++;
+			Debug.Log("arvo: " + x);
+			Conversation();
+		}
+
+		public void Stop() {
+			conversation.SetActive(false);
+		}
 }
